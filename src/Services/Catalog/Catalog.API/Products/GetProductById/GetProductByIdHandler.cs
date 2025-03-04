4ffb7d9f393a01a7767a7550b7f9945c8b65e336
Tencho <tencho1011@gmail.com>
2025-03-04 16:35:44 +0300

    Develop GetProductById GET Endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,10 +1,10 @@
 ﻿namespace Catalog.API.Products.GetProductById
 {
-    public record GetProductByIdQuery(Guid Id): IQuery<GetProductByIdResult>;
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
 
     public record GetProductByIdResult(Product Product);
 
-    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger logger)
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
         : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
