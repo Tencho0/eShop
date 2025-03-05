10ba9b674a17e7f51ce2f099ddb1845f84b6ed31
Tencho <tencho1011@gmail.com>
2025-03-05 12:24:15 +0300

    Create Global Exception Handling

--- src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -1,8 +1,11 @@
 ﻿namespace Catalog.API.Exceptions
 {
-    public class ProductNotFoundException: Exception
+    using BuildingBlocks.Exceptions;
+
+    public class ProductNotFoundException : NotFoundException
     {
-        public ProductNotFoundException(): base("Product not found!")
+        public ProductNotFoundException(Guid Id)
+            : base("Product", Id)
         {
 
         }
