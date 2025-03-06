a7744012fab0f1fc701862782a6065f4e4ad287a
Tencho <tencho1011@gmail.com>
2025-03-06 10:10:13 +0300

    Implement IBasketRepository

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,8 +1,11 @@
 ﻿global using Carter;
+global using Marten;
 global using MediatR;
 global using Mapster;
 global using FluentValidation;
 
 global using Basket.API.Models;
+global using Basket.API.Exceptions;
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
