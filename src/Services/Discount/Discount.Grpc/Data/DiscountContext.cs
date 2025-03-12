cf7863e4198d7d58dc38b1d92c4b0cbc5a5d520b
Tencho <tencho1011@gmail.com>
2025-03-12 15:24:26 +0200

    Seed and AutoMigrate SQLite Database

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -10,7 +10,13 @@
         public DiscountContext(DbContextOptions<DiscountContext> options)
             : base(options)
         {
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhoneDiscount", Amount = 150 },
+                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 });
         }
     }
 }
