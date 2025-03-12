d6e799813f70577f94fe750f60773ffae4515e86
Tencho <tencho1011@gmail.com>
2025-03-12 14:58:57 +0200

    Create DbContext & SQLite Database

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Discount.Grpc.Data
+{
+    using Discount.Grpc.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,25 @@
+﻿namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoServices.DiscountProtoServicesBase
+    {
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, global::Grpc.Core.ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
