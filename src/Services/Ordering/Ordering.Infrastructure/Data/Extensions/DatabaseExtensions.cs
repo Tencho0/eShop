69b5da11b09ef0d4e0829740e7c891ec4c171457
Tencho <tencho1011@gmail.com>
2025-03-21 14:59:44 +0200

    Auto Migrate when Application Startup

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Infrastructure.Data.Extensions
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DatabaseExtensions
+    {
+        public static async Task InitialiseDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var conntext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            conntext.Database.MigrateAsync().GetAwaiter().GetResult();
+        }
+    }
+}
