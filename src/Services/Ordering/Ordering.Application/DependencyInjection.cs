68c9f8e30f67521894930813eec0e745b4a5cd6d
Tencho <tencho1011@gmail.com>
2025-04-03 09:50:35 +0300

    Create Domain Event Handlers

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,15 +1,18 @@
 ﻿namespace Ordering.Application
 {
     using System.Reflection;
+    using BuildingBlocks.Behaviors;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddMediatR(cfg =>
+            services.AddMediatR(config =>
             {
-                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehaviors<,>));
             });
 
             return services;
