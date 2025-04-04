79fdb2464eda5b7807625fbab461e6b60d3c9f2b
Tencho <tencho1011@gmail.com>
2025-04-04 17:18:12 +0300

    Develop GetOrders Endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,28 @@
 ﻿namespace Ordering.API.Endpoints
 {
-    public class GetOrders
+    using BuildingBlocks.Pagination;
+    using Ordering.Application.Orders.Queries.GetOrders;
+
+    public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+    public class GetOrders : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersQuery(request));
+
+                var response = result.Adapt<GetOrdersResponse>();
+
+                return Results.Ok(response);
+            })
+           .WithName("GetOrders")
+           .Produces<CreateOrderResponse>(StatusCodes.Status200OK)
+           .ProducesProblem(StatusCodes.Status400BadRequest)
+           .ProducesProblem(StatusCodes.Status404NotFound)
+           .WithSummary("Get Orders")
+           .WithDescription("Get Orders");
+        }
     }
 }
