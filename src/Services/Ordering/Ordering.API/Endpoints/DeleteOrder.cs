9a043e0c8f3c341b2f4e86ec90e9a4bfeb227536
Tencho <tencho1011@gmail.com>
2025-04-04 16:57:21 +0300

    Develop DeleteOrder Endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,7 +1,26 @@
 ﻿namespace Ordering.API.Endpoints
 {
-    public class DeleteOrder
-    {
+    using Ordering.Application.Orders.Commands.DeleteOrder;
 
+    public record DeleteOrderResponse(bool IsSuccess);
+
+    public class DeleteOrder : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteOrderCommand(Id));
+
+                var response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteOrder")
+            .Produces<CreateOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Order")
+            .WithDescription("Delete Order");
+        }
     }
 }
