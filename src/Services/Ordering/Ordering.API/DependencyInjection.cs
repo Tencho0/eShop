58c5bd204028a735a514d63157d4b8aa6ffc0a05
Tencho <tencho1011@gmail.com>
2025-04-08 15:52:27 +0300

    Add HealthChecks and Exception Handling

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,12 +1,17 @@
 ﻿namespace Ordering.API
 {
-    using Carter;
+    using BuildingBlocks.Exceptions.Handler;
+    using HealthChecks.UI.Client;
+    using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddCarter();
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
@@ -14,6 +19,12 @@
         public static WebApplication UseApiServices(this WebApplication app)
         {
             app.MapCarter();
+            app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
 
             return app;
         }
