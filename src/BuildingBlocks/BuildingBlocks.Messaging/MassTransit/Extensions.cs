ae2ea504fa4ab7b16506df0b309477332777fc14
Tencho <tencho1011@gmail.com>
2025-04-10 17:08:36 +0300

    Introduce Messages

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,34 @@
+﻿namespace BuildingBlocks.Messaging.MassTransit
+{
+    using System.Reflection;
+    using Microsoft.Extensions.DependencyInjection;
+    using global::MassTransit;
+    using Microsoft.Extensions.Configuration;
+
+    public static class Extensions
+    {
+        public static IServiceCollection AddMessageBroker
+            (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+        {
+            services.AddMassTransit(config =>
+            {
+                config.SetKebabCaseEndpointNameFormatter();
+
+                if (assembly != null)
+                    config.AddConsumers(assembly);
+
+                config.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                    {
+                        host.Username(configuration["MessageBroker:UserName"]);
+                        host.Password(configuration["MessageBroker:Password"]);
+                    });
+                    configurator.ConfigureEndpoints(context);
+                });
+            });
+
+            return services;
+        }
+    }
+}
